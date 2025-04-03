3b8f5559eb71e0c47b0e8e0430c003a6d29b7473
TheNocturnalDevGypsy <i.am.abby@thenocturnaldevgypsy.io>
2025-04-03 23:20:20 +0700

    Updated repo's index.html, bsct-main.css and bsct-main.js. Part to resolve issue #9

---
+++ assets/js/bsct-core.js
@@ -0,0 +1,6 @@
+/** ============================================================================================================================
+* Version and Initial Release: Version 1.0.0 with Bootstrap v5.3.3
+* Author: github.com/TheNocturnalDevGypsy-io [ Abegail Baustista Torrendon ]
+* GitHub Source: https://github.com/thenocturnaldevgypsy/bootstrap-coded-templates
+* License: MIT License - https://github.com/thenocturnaldevgypsy/bootstrap-coded-templates/blob/master/LICENSE
+============================================================================================================================== **/
